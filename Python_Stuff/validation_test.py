73d7a65d2d68de11ef32c51a600497273b530195
Kay <k.dgray23@gmail.com>
2016-09-01 17:40:22 -0400

    Add files via upload

---
+++ Python_Stuff/validation_test.py
@@ -0,0 +1,20 @@
+months = ['January',
+          'February',
+          'March',
+          'April',
+          'May',
+          'June',
+          'July',
+          'August',
+          'September',
+          'October',
+          'November',
+          'December']
+          
+def valid_month(month):
+    if month in months or month.title() in months:
+        return month.title()
+    
+
+
+print(valid_month('gg'))
